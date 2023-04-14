b7d4c0693ad147d4a5addd6b0b9e661cf7cdec31
lipovsek-aws <117850701+lipovsek-aws@users.noreply.github.com>
2023-04-14 15:44:44 +0100

    Added pytorch test

--- pyxis/test.sh
+++ pyxis/test.sh
@@ -3,3 +3,4 @@ srun --container-image=alpine grep PRETTY /etc/os-release
 sbatch --container-image=alpine --wrap "grep PRETTY /etc/os-release"
 sbatch --wrap "srun --container-image=alpine grep PRETTY /etc/os-release"
 srun --container-image=nvidia/cuda:11.6.2-base-ubuntu20.04 nvidia-smi
+srun --container-image=pytorch/pytorch:1.13.1-cuda11.6-cudnn8-devel python -c "import torch;print(torch.cuda.is_available())"
