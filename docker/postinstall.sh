c358504bfe0ee9edc375aa5b3ce85149eb4e5e5c
wdykas <wdykas@nvidia.com>
2023-05-11 10:46:03 -0700

    try adding everyone to group

--- docker/postinstall.sh
+++ docker/postinstall.sh
@@ -28,9 +28,11 @@ elif [ "${OS}" = "Ubuntu" ]; then
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get -y update
     apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    groupadd docker
-    usermod -aG docker "ubuntu"
-    newgrp docker
+    #groupadd docker
+    chgrp docker $(which docker)
+    chmod g+s $(which docker)
+    #usermod -aG docker ubuntu
+    #newgrp docker
     systemctl enable docker.service
     systemctl start docker.service
 
