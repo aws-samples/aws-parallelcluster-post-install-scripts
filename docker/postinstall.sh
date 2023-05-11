c774a6cdaec82aaf04e40515011fe5294f262c0f
wdykas <wdykas@nvidia.com>
2023-05-11 12:15:11 -0700

    alinux support

--- docker/postinstall.sh
+++ docker/postinstall.sh
@@ -11,7 +11,8 @@ if [ "${OS}" = "Amazon Linux" ]; then
     yum install docker
     usermod -a -G ec2-user
     id ec2-user
-    newgrp docker
+    chgrp docker $(which docker)
+    chmod g+s $(which docker)
     systemctl enable docker.service
     systemctl start docker.service
 elif [ "${OS}" = "Ubuntu" ]; then
@@ -28,11 +29,8 @@ elif [ "${OS}" = "Ubuntu" ]; then
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get -y update
     apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    #groupadd docker
     chgrp docker $(which docker)
     chmod g+s $(which docker)
-    #usermod -aG docker ubuntu
-    #newgrp docker
     systemctl enable docker.service
     systemctl start docker.service
 
