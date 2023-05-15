68256e139616be1ab2c1d77557cf7abbcccfc859
wdykas <73254672+wdykas@users.noreply.github.com>
2023-05-15 18:05:04 -0400

    Docker install (#5)
    * initial docker install commit
    
    * fixes
    
    * try adding everyone to group
    
    * alinux support
    
    * alinux fixes

---
+++ docker/postinstall.sh
@@ -0,0 +1,38 @@
+# !/bin/bash
+set -e
+
+
+OS=$(. /etc/os-release; echo $NAME)
+
+if [ "${OS}" = "Amazon Linux" ]; then
+    yum -y update
+    yum search docker
+    yum info docker
+    yum -y install docker
+    chgrp docker $(which docker)
+    chmod g+s $(which docker)
+    systemctl enable docker.service
+    systemctl start docker.service
+elif [ "${OS}" = "Ubuntu" ]; then
+    apt-get -y update
+    apt-get -y install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+    mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get -y update
+    apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    chgrp docker $(which docker)
+    chmod g+s $(which docker)
+    systemctl enable docker.service
+    systemctl start docker.service
+
+else
+        echo "Unsupported OS: ${OS}" && exit 1;
+fi
+
