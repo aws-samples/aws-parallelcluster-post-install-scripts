11de5fbdddaf383971f175108703872918e7964d
Ryan Kilpadi <rkilpadi@amazon.com>
2023-07-11 14:35:53 -0400

    Rotate JWT token using cron for Slurm REST API

--- rest-api/postinstall.sh
+++ rest-api/postinstall.sh
@@ -3,25 +3,29 @@
 set -x
 set -e
 
-mkdir -p /tmp/slurm_rest_api
-pushd /tmp/slurm_rest_api
+# Copy Slurm REST API configuration files and scripts
+tmp_dir=/tmp/slurm_rest_api
+mkdir -p $tmp_dir
 
-# Copy Slurm configuration files
 source_path=https://raw.githubusercontent.com/aws-samples/aws-parallelcluster-post-install-scripts/main/rest-api
-files=(slurmrestd.service slurm_rest_api.rb nginx.conf nginx.repo.erb)
+files=(slurmrestd.service slurm_rest_api.rb nginx.conf)
 for file in "${files[@]}"
 do
-    wget -qO- ${source_path}/${file} > ${file}
+    wget -qO- $source_path/$file > $tmp_dir/$file
 done
 
+rotate_jwt_path=/opt/parallelcluster/scripts/rotate_jwt.sh
+wget -qO- $source_path/rotate_jwt.sh > $rotate_jwt_path
+chmod +x $rotate_jwt_path
+
+# Setup Slurm REST API
 sudo cinc-client \
   --local-mode \
   --config /etc/chef/client.rb \
   --log_level auto \
   --force-formatter \
-  --no-color \
   --chef-zero-port 8889 \
   -j /etc/chef/dna.json \
-  -z slurm_rest_api.rb
+  -z $tmp_dir/slurm_rest_api.rb
 
 set +e
