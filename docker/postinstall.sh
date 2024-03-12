d5aa5bf1a3ae77d28871f5f9fa9bed032657545d
mhuguesaws <71357145+mhuguesaws@users.noreply.github.com>
2024-03-12 13:20:23 -0500

    Add nvidia container toolkit (#17)
    Close #16

--- docker/postinstall.sh
+++ docker/postinstall.sh
@@ -47,3 +47,38 @@ echo "
 # END: post-install docker
 ###################################
 "
+
+echo "
+##############################################
+# BEGIN: post-install nvidia-container-toolkit
+##############################################
+"
+
+OS=$(. /etc/os-release; echo $NAME)
+
+if [ "${OS}" = "Amazon Linux" ]; then
+    curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
+    tee /etc/yum.repos.d/nvidia-container-toolkit.repo
+    yum install -y nvidia-container-toolkit
+
+elif [ "${OS}" = "Ubuntu" ]; then
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+    apt-get -y update
+    apt-get install -y nvidia-container-toolkit
+
+else
+        echo "Unsupported OS: ${OS}" && exit 1;
+fi
+
+nvidia-ctk runtime configure --runtime=docker
+systemctl restart docker
+
+echo "
+############################################
+# END: post-install nvidia-container-toolkit
+############################################
+"
